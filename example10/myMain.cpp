180f4a1c63f557f286bba973bfaee8a5ecc14b62
Mark Grimes <mark.grimes@rymapt.com>
2018-07-18 11:33:17 +0100

    Fix compile error after I changed a member name in only one place

--- example10/myMain.cpp
+++ example10/myMain.cpp
@@ -8,9 +8,9 @@ struct MyData
 unsigned long long myMain()
 {
 	implbasic::impl::NDimensionalBinner<MyData,true,double,double> binner( 10, -0.5, 9.5, 5, -2, 2 );
-	++binner.get(0,0).age;
-	++binner.get(0,-0.1).age;
-	++binner.get(0,0.1).age;
-	++binner.get(0,3).age;
-	return binner.get(0,0).age;
+	++binner.get(0,0).someData;
+	++binner.get(0,-0.1).someData;
+	++binner.get(0,0.1).someData;
+	++binner.get(0,3).someData;
+	return binner.get(0,0).someData;
 }
