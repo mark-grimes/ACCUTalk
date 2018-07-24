e996df119df6406a44c691c427fdafd5f88b2554
Mark Grimes <mark.grimes@rymapt.com>
2018-07-24 16:29:10 +0100

    Change example09 to be slightly more practical

--- example09_compiletime_cpp11/myMain.cpp
+++ example09_compiletime_cpp11/myMain.cpp
@@ -1,16 +1,25 @@
 #include "NDimensionalBinner.h"
 
-struct MyData
+struct HistogramBin
 {
-	int someData=0;
+	float totalWeight=0;
+	unsigned entries=0;
+	float sumOfWeightsSquared=0; // sqrt of this will give the poisson error of bin height
 };
 
 unsigned long long myMain()
 {
-	implbasic::impl::NDimensionalBinner<MyData,true,double,double> binner( 10, -0.5, 9.5, 5, -2, 2 );
-	++binner.get(0,0).someData;
-	++binner.get(0,-0.1).someData;
-	++binner.get(0,0.1).someData;
-	++binner.get(0,3).someData;
-	return binner.get(0,0).someData;
+	implbasic::impl::NDimensionalBinner<HistogramBin,true,double,double> binner( 10, -0.5, 9.5, 5, -2, 2 );
+
+	std::vector<std::pair<double,double>> exampleData{ {0,0}, {0,-0.1}, {0,0.1}, {0,3} };
+	for( const auto& datapoint : exampleData )
+	{
+		HistogramBin& bin=binner.get(datapoint.first,datapoint.second);
+		constexpr float weight=1; // Could weight entries, but don't bother in this example
+		bin.totalWeight+=weight;
+		bin.entries+=1;
+		bin.sumOfWeightsSquared+=weight*weight;
+	}
+
+	return binner.get(0,0).totalWeight;
 }
