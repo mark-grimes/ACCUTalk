1b59e71d97ed10700bb75685aa56c6f9813de661
Mark Grimes <mark.grimes@rymapt.com>
2018-07-24 13:53:50 +0100

    Add tuple code to the tuple example main

--- example06_tupleimplementation_cpp11/myMain.cpp
+++ example06_tupleimplementation_cpp11/myMain.cpp
@@ -2,5 +2,9 @@
 
 unsigned long long myMain()
 {
-	return 1+1*2;
+	mytuple<float,char,int> foo;
+	foo.method_get<0>()=2.3;
+	foo.method_get<1>()='F';
+	foo.method_get<2>()=100;
+	return foo.method_get<2>();
 }
