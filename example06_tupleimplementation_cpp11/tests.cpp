4f5ed8b40a736b5d47f565e0966106845b8a758d
Mark Grimes <mark.grimes@rymapt.com>
2018-07-24 13:46:36 +0100

    Move code using tuple to where it's more relevant

--- example06_tupleimplementation_cpp11/tests.cpp
+++ example06_tupleimplementation_cpp11/tests.cpp
@@ -15,6 +15,14 @@ SCENARIO( "Tests for the mytyple_typehelper struct" )
 SCENARIO( "Tests for the live coding at the ACCU talk" )
 {
 	mytuple<float,char,int> instance;
+	instance.method_get<0>()=2.3;
+	instance.method_get<1>()='F';
+	instance.method_get<2>()=87;
+	CHECK( instance.method_get<0>()==Approx(2.3) );
+	CHECK( instance.method_get<1>()=='F' );
+	CHECK( instance.method_get<2>()==87 );
+
+
 	CHECK( std::is_same<mytuple<float,char,int>::type<0>,float>::value );
 	CHECK( std::is_same<mytuple<float,char,int>::type<1>,char>::value );
 	CHECK( std::is_same<mytuple<float,char,int>::type<2>,int>::value );
@@ -26,12 +34,6 @@ SCENARIO( "Tests for the live coding at the ACCU talk" )
 	/* Show that this is all calculated at compile time with C++11 static_assert */
 	static_assert( std::is_same<mytuple<float,char,int>::type<0>,float>::value, "types are not equal" );
 
-	instance.method_get<0>()=2.3;
-	instance.method_get<1>()='F';
-	instance.method_get<2>()=87;
-	CHECK( instance.method_get<0>()==Approx(2.3) );
-	CHECK( instance.method_get<1>()=='F' );
-	CHECK( instance.method_get<2>()==87 );
 
 	/* This should fail to compile because there aren't enough template parameters */
 	// instance.method_get<3>()=87;
